b6fcf2e6741e2513cf4697c282ce437b03d9df1c
Matthew Carter <m@ahungry.com>
2020-02-25 16:28:23 -0500

    adjust to use promise based

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 
 const { app } = require('./lib/app')
+const mw = require('./lib/mw')
 const { proxy } = require('./lib/proxy')
 const { transform } = require('./lib/transform')
 
@@ -8,8 +9,16 @@ console.log('Running on port 12345')
 console.log('Try visiting http://0.0.0.0:12345')
 
 // cb of the err, data format
-function slurp (filename, cb) {
-  fs.readFile(filename, { encoding: 'utf-8' }, cb)
+async function slurp (filename) {
+  const p = await new Promise((resolve, reject) => {
+    fs.readFile(filename, { encoding: 'utf-8' }, (err, data) => {
+      if (err) reject(err)
+
+      resolve(data)
+    })
+  })
+
+  return p
 }
 
 // Wrap a function around the callback
@@ -39,6 +48,7 @@ function postProxy (cb) {
   // curl http://0.0.0.0:12345/https://httpbin.org/post -XPOST -d '{"sentinel": 1}' -D-
   const mapping = [
     ['headers.Host', 'my-host'],
+    ['origin', 'my-origin'],
   ]
   const t = transform(mapping)
 
@@ -47,29 +57,72 @@ function postProxy (cb) {
 
     const x = t(JSON.parse(data))
 
-    cb(err, JSON.stringify(x))
+    console.log('PROXY AFTER TRANSFORM IS: ', { x })
+
+    cb(err, {
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: x
+    })
+    // cb(err, JSON.stringify(x))
   }
 }
 
-function handler (req = undefined) {
-  const { url } = req
+function route (handler) {
+  return async function (req) {
+    const res = await handler(req)
+    const { url } = req
 
-  switch (url) {
-    case '/version': return cb => cb(undefined, JSON.stringify({ version: '0.0.1' }))
+    switch (url) {
+      case '/version':
+        return {
+          ...res,
+          headers: {
+            ...res.headers,
+            'content-type': 'application/json',
+          },
+          body: {
+            version: '0.0.1',
+          }
+        }
 
-    // Just load the basic file.
-    case '/iosevka-term-regular.woff':
-    case '/index.js':
-    case '/main.css':
-      return cb => slurp(url.slice(1), cb)
+        // Just load the basic file.
+      case '/iosevka-term-regular.woff':
+      case '/index.js':
+      case '/main.css':
+        return {
+          ...res,
+          body: await slurp(url.slice(1)),
+        }
+        // return cb => slurp(url.slice(1), cb)
 
-    case '/':
-      return cb => slurp('index.html', cb)
-    default:
-      return /\/http/.test(url)
-        ? cb => proxy(url, req, postProxy(cb))
-        : cb => slurp('index.html', cb)
+      case '/':
+        return {
+          ...res,
+          body: await slurp('index.html')
+        }
+
+      default:
+        return /\/http/.test(url)
+          ? await evalProxy(url, req)
+          : { ...res, body: await slurp('index.html') }
+    }
   }
 }
 
-app(12345, wrapJson(wrapLog(handler)))
+async function evalProxy (url, req) {
+  const result = await new Promise((resolve, reject) => {
+    proxy(url, req, postProxy((err, data) => {
+      if (err) reject(err)
+      resolve(data)
+    }))
+  })
+
+  console.log('evalProxy result is: ', { result })
+
+  return result
+}
+
+// app(12345, wrapJson(wrapLog(handler)))
+app(12345, mw.wrapCors(mw.wrapJsonReq(route(mw.init))))
